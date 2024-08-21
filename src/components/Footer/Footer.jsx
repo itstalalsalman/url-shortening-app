e2d8e9b877d439a6f54ddda718fef8641278a4a6
itstalalsalman <mustangcreator@gmail.com>
2024-08-21 19:36:07 +0500

    Footer Completed Along with Zustand store for state management initiated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,11 @@ import './Footer.css';
 import ListComponent from '../ListComponent/ListComponent';
 
 import logo from '../../assets/logoFooter.svg';
+import facebook from '../../assets/icon-facebook.svg';
+import twitter from '../../assets/icon-twitter.svg';
+import pinterest from '../../assets/icon-pinterest.svg';
+import instagram from '../../assets/icon-instagram.svg';
+
 
 const Footer = () => {
     
@@ -12,13 +17,23 @@ const Footer = () => {
         {listName: "Compnay", listItemsNames: ["About", "Our Team", "Careers", "Contacts"]}
     ];
 
+    const icons = [
+        facebook,
+        twitter,
+        pinterest,
+        instagram
+    ];
+
   return (
     <div className="footer">
         <img src={logo} alt='logo' className='logo-footer'/>
         <div className='listing-and-icons'>
-            {footerlisting.map((list) => {
+            {footerlisting.map((list) => (
                 <ListComponent liName={list.listName} liItems={list.listItemsNames} />
-            })}
+            ))}
+            <div className='icons-container'>
+                {icons.map(icon => <img src={icon} alt='footer-icon' className='icon-footer' />)}
+            </div>
         </div>
     </div>
   )
