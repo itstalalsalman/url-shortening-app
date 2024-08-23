05edc2209928f91003166acf5a6524ea688673f2
itstalalsalman <mustangcreator@gmail.com>
2024-08-24 01:56:07 +0500

    Framer motion Added with some motion to hero page along with css keyframes

--- src/pages/HeroScreen/HeroScreen.jsx
+++ src/pages/HeroScreen/HeroScreen.jsx
@@ -1,12 +1,29 @@
 import './HeroScreen.css'
+import { motion } from 'framer-motion'
+
 import heroImg from '../../assets/illustration-working.svg'
 
+
 const HeroScreen = () => {
+
+  
   return (
     <div className='hero-main'>
         <div className='info-container'>
-            <h1>More than just shorter links</h1>
-            <p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+            <motion.h1
+              initial={{ x: '-50vw' }}  // Starts from the right
+              animate={{ x: 0 }}  // Animates to its place
+              transition={{ type: 'spring', stiffness: 80, duration: 0.5 }}
+            >
+              More than just shorter links
+            </motion.h1>
+            <motion.p
+              initial={{ x: '-50vw' }}  // Starts from the right
+              animate={{ x: 0 }}  // Animates to its place
+              transition={{ type: 'spring', stiffness: 80, duration: 0.5 }}
+            >
+              Build your brand's recognition and get detailed insights on how your links are performing.
+            </motion.p>
             <button className='green-hover'>Get Started</button>
         </div>
         <div className='image-container'>
